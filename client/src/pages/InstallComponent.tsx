3f108f00070243f6fc2fd9e4c9bc44a41d5a48fd
Steven McKeown <steven@pragmaflow.com>
2021-12-05 13:04:14 -0500

    Update InstallComponent.tsx

--- client/src/pages/InstallComponent.tsx
+++ client/src/pages/InstallComponent.tsx
@@ -15,6 +15,7 @@ import LinkIcon from '@mui/icons-material/Link';
 import MailchimpSubscribe from "react-mailchimp-subscribe" 
 import TranslateIcon from '@mui/icons-material/Translate';
 import LanguageIcon from '@mui/icons-material/Language';
+import ShareIcon from '@mui/icons-material/Share';
 
 const url = "https://pragmaflow.us20.list-manage.com/subscribe/post?u=e0b0c6210f007d12879685ac3&amp;id=01a6a73410";
 
@@ -25,6 +26,7 @@ const InstallableComponents = [{
   libraryName: "pf-geo-location-steven",
   description: "This component allows reading the current location information of the device your app is running on.",
   githubUrl: "https://github.com/pragmaflowinc/adalo-geolocation",
+  youtubeUrl: "",
   icon: <LocationIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "8d471b8f-b04c-4405-aa65-88b53479a52b",
@@ -32,6 +34,7 @@ const InstallableComponents = [{
   libraryName: "pragmaflow-biometrics",
   description: "This component allows users to login to their Adalo account using biometrics on supported devices.",
   githubUrl: "https://github.com/pragmaflowinc/adalo-biometric-login",
+  youtubeUrl: "",
   icon: <FingerprintIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "0b6ea620-0419-4eb3-baad-f13d5af5a605",
@@ -39,6 +42,7 @@ const InstallableComponents = [{
   libraryName: "phone-contacts",
   description: "This component allows users to access their phone contact list within an Adalo app.",
   githubUrl: "https://github.com/pragmaflowinc/adalo-phone-contacts",
+  youtubeUrl: "",
   icon: <ContactsIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "944b88fc-6630-4467-8622-ff34b6ef42df",
@@ -46,6 +50,7 @@ const InstallableComponents = [{
   libraryName: "adalo-nfc",
   description: "This component allows users to have the ability to read Nfc tag ids.",
   githubUrl: "https://github.com/pragmaflowinc/adalo-nfc",
+  youtubeUrl: "",
   icon: <NfcIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "e5c223ff-3cd8-4e4f-b2c0-293122969997",
@@ -53,13 +58,15 @@ const InstallableComponents = [{
   libraryName: "adalo-local-storage",
   description: "This component allows app designers to persist data on the phone via local storage.",
   githubUrl: "https://github.com/pragmaflowinc/adalo-local-storage",
+  youtubeUrl: "",
   icon: <StorageIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "fb55da5a-76a5-435b-bde8-a4f6a673e4ab",
   name: "Encryption Toolkit",
   libraryName: "pf-encryption-toolkit",
   description: "This suite of components facilitate encypting and decrypting data being saved in the Adalo database.",
-  githubUrl: "https://github.com/pragmaflowinc/adalo-encrypted-data",
+  githubrl: "https://github.com/pragmaflowinc/adalo-encrypted-data",
+  youtubeUrl: "",
   icon: <HttpsIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "5c55a27d-d88d-45c8-b321-0b263637134f",
@@ -67,6 +74,7 @@ const InstallableComponents = [{
   libraryName: "pf-adalo-data-preloader",
   description: "This is a utility that will allow you to preload the data for your screens.",
   githubUrl: "https://github.com/pragmaflowinc/adalo-data-preloader",
+  youtubeUrl: "",
   icon: <CachedIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "9bce3f23-9a32-4f70-b7db-698fbd0085c8",
@@ -74,6 +82,7 @@ const InstallableComponents = [{
   libraryName: "adalo-ably",
   description: "This allows you to create websocket connections between apps for real-time data sharing.",
   githubUrl: "https://github.com/pragmaflowinc/adalo-ably-sockets",
+  youtubeUrl: "",
   icon: <CompareArrowsIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "7d081ba1-7c10-4ac1-87de-9b0b8b2414dc",
@@ -81,6 +90,7 @@ const InstallableComponents = [{
   libraryName: "adalo-timer-action",
   description: "This allows you to create repeating actions using a timer.",
   githubUrl: "https://github.com/pragmaflowinc/adalo-timer-action",
+  youtubeUrl: "",
   icon: <TimerIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "3d55348d-92ef-4eeb-b6ee-c469b567e257",
@@ -88,6 +98,7 @@ const InstallableComponents = [{
   libraryName: "driving-direction",
   description: "This is a map that lets you build driving directions between two destinations.",
   githubUrl: "https://github.com/pragmaflowinc/adalo-driving-direction",
+  youtubeUrl: "",
   icon: <DirectionsIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "fa9c7d1e-34e2-4549-873c-92f124dffe76",
@@ -95,21 +106,32 @@ const InstallableComponents = [{
   libraryName: "adalo-deep-links",
   description: "This will allow you to add deep links into your native apps (not PWA).",
   githubUrl: "https://github.com/pragmaflowinc/adalo-deepish-linking",
+  youtubeUrl: "",
   icon: <LinkIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "2d34d4cc-54d0-47a1-b87e-e90ff3ac838d",
   name: "Localization",
   libraryName: "adalo-localize",
   description: "This reads the localization information (language preference, country, on metric system, etc..) of the user. It works really well in Native, it works okay in web.",
-  githubUrl: "https://github.com/pragmaflowinc/adalo-deepish-linking",
+  githubUrl: "https://github.com/pragmaflowinc/adalo-localize",
+  youtubeUrl: "",
   icon: <LanguageIcon sx={{ fontSize: 196}}  color="primary" />
 },{
   id: "9ecd7d3a-1ad0-4506-b63e-622ab24eafbd",
   name: "Multi-language Translator",
   libraryName: "adalo-multilanguage",
   description: "Allows the auto-magic updating of all logged in user words in your app.",
-  githubUrl: "https://github.com/pragmaflowinc/adalo-deepish-linking",
+  githubUrl: "https://github.com/pragmaflowinc/adalo-multilanguage",
+  youtubeUrl: "",
   icon: <TranslateIcon sx={{ fontSize: 196}}  color="primary" />
+},{
+  id: "c98b6c44-4897-4b7c-a8d9-a901e24caad2",
+  name: "Share on Social Media",
+  libraryName: "adalo-share-on-social-media",
+  description: "Allows end users to share stuff on the large social media platforms.",
+  githubUrl: "https://github.com/pragmaflowinc/adalo-social-share",
+  youtubeUrl: "",
+  icon: <ShareIcon sx={{ fontSize: 196}}  color="primary" />
 }]
 
 interface OrgType {
