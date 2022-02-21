e94c605859cb51653895719bc193ba828aac37fd
Dave Diplock <dave@pragmaflow.com>
2022-02-21 12:37:16 -0500

    Adding feedback mailer, template, nodemailer

--- server/src/graphql/Feedback/Feedback.resolver.ts
+++ server/src/graphql/Feedback/Feedback.resolver.ts
@@ -1,8 +1,16 @@
 import { Arg, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import { Service } from "typedi";
 import { ToDoList } from "../../entity/ToDoList";
-import { createTransport } from 'nodemailer'
 import { Feedback } from "../../entity/Feedback";
+
+import fs from 'fs';
+import path from 'path';
+import mustache from 'mustache';
+import mjml from 'mjml';
+
+import { mailClient } from '../../utils/email'
+
+const templateFeedback = fs.readFileSync(path.join(__dirname, "../../email-templates/feedback-submitted.mjml"), "utf8");
 
 @Service()
 @Resolver()
@@ -22,6 +30,22 @@ export class FeedbackResolver {
         subject,
         content
       }).save()
+
+      const renderedEmail = mustache.render(templateFeedback, { 
+        email, 
+        subject, 
+        content,
+        createdAt: new Date()
+      });
+      const html = mjml(renderedEmail).html;
+
+      await mailClient.sendMail({
+        from: "info+adalo-components@pragmaflow.com",
+        to:"info@pragmaflow.com",
+        subject: "Adalo Components Feedback Submitted",
+        html
+      });
+
       return true
     } catch {
 
---
+++ server/src/utils/email.ts
@@ -0,0 +1,35 @@
+import { createTransport } from 'nodemailer'
+import { EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS } from './config'
+
+// Limits to be able to send bulk mails to MailTrap
+const limiting = {
+    pool: true,
+    rateLimit: 3,
+    rateDelta: 20_000,
+    maxConnections: 1,
+    maxMessages: 3,
+}
+
+const transportData = {
+    host: EMAIL_HOST,
+    port: EMAIL_PORT,
+    // secure: true, // use TLS
+    auth: {
+        user: EMAIL_USER,
+        pass: EMAIL_PASS,
+    },
+}
+
+//const dataToUse = ENVIRONMENT !== 'app' ? Object.assign(transportData, limiting) : transportData
+
+export const mailClient = createTransport(transportData)
+
+export const getReturnPathOptions = (toEmail: string) => ({
+    headers: {
+        'Return-Path': '<info@pragmaflow.com>',
+    },
+    envelope: {
+        'from': '<info@pragmaflow.com>',
+        'to': toEmail
+    }
+})
