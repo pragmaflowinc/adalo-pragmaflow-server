ed7a474bdfdcda0624eec3ab290822ea85c551b7
Mitch <72631198+Mitch-PragmaFlow@users.noreply.github.com>
2022-08-15 17:02:04 -0400

    added warning

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import { Box, Typography } from '@mui/material'
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
 export function Welcome() {
   return (
     <Box>
       <Typography variant="h1">Happy you came!</Typography>
-      <Typography>Hello from PragmaFlow Inc. and thanks for stopping in. It was a little touch an go for awhile there when we started getting off the ground. A couple of shut-downs, an uncertain future, but thanks to community support and some vocal activists we are back online, seemingly for good.</Typography>
-      <Typography>With that said, we are confident we can invest more time into making this the single best place to answer all of your Adalo questions. Come back regularly, I am sure there will be something new and exiciting to see.</Typography>
-      <Typography>If you feel up to it, and like what we are doing, feel free to click on the "Become a patron" button to support us!</Typography>
+      <script>unmaintainedWarning();</script>
+      <br />
+      <Typography>
+        Hello from PragmaFlow Inc. and thanks for stopping in. It was a little
+        touch an go for awhile there when we started getting off the ground. A
+        couple of shut-downs, an uncertain future, but thanks to community
+        support and some vocal activists we are back online, seemingly for good.
+      </Typography>
+      <br />
+      <br />
+      <Typography>
+        If you feel up to it, and like what we are doing, feel free to click on
+        the "Become a patron" button to support us!
+      </Typography>
     </Box>
-  )
+  );
 }
