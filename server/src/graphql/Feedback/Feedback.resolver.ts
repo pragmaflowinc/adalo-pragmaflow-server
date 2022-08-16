05eb4cdde08ce3a968ba79b2ca2364fc84a5c761
Steven McKeown <steven@pragmaflow.com>
2022-08-16 09:06:00 -0400

    Revert "Revert "Merge pull request #1 from pragmaflowinc/features/emailer""
    This reverts commit 86dfa82d6a390adce8fb1d97f1c52ebdc3eadf1f.

--- server/src/graphql/Feedback/Feedback.resolver.ts
+++ server/src/graphql/Feedback/Feedback.resolver.ts
@@ -1,8 +1,17 @@
 import { Arg, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import { Service } from "typedi";
 import { ToDoList } from "../../entity/ToDoList";
-import { createTransport } from 'nodemailer'
 import { Feedback } from "../../entity/Feedback";
+
+import fs from 'fs';
+import path from 'path';
+import mustache from 'mustache';
+import mjml from 'mjml';
+
+import { mailClient } from '../../utils/email'
+import { NOTIFICATION_EMAIL } from '../../utils/config'
+
+const templateFeedback = fs.readFileSync(path.join(__dirname, "../../email-templates/feedback-submitted.mjml"), "utf8");
 
 @Service()
 @Resolver()
@@ -13,15 +22,31 @@ export class FeedbackResolver {
     @Arg("email", { nullable: true }) email?: string,
     @Arg("subject", { nullable: true }) subject?: string
   ) {
-    console.log(content)
-    console.log(email)
-    console.log(subject)
+    //console.log(content)
+    //console.log(email)
+    //console.log(subject)
     try {
       await Feedback.create({
         email,
         subject,
         content
       }).save()
+
+      const renderedEmail = mustache.render(templateFeedback, { 
+        email, 
+        subject, 
+        content,
+        createdAt: new Date()
+      });
+      const html = mjml(renderedEmail).html;
+
+      const result = await mailClient.sendMail({
+        from: "Adalo PragmaflowServers <adalo.pragmaflow@gmail.com>",
+        to: NOTIFICATION_EMAIL,
+        subject: "Adalo Components Feedback Submitted",
+        html
+      });
+
       return true
     } catch {
 
